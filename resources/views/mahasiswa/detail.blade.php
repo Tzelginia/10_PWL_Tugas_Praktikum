1913c7b4d1b4c3767c8bc7381b04b28758a3f7e0
tzelginianc <tzelginia78@gmail.com>
2022-04-24 15:33:59 +0700

    Tugas 1 Prak 10 (detail kolom foto)

--- resources/views/mahasiswa/detail.blade.php
+++ resources/views/mahasiswa/detail.blade.php
@@ -10,6 +10,8 @@
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item"><b>Nim: </b3>{{$Mahasiswa->nim}}</li>
                     <li class="list-group-item"><b>Nama: </b>{{$Mahasiswa->nama}}</li>
+                    <li class="list-group-item"><b>Foto: </b><img width="100px" src="{{asset('storage/'.$Mahasiswa->foto)}}">
+                    </li>
                     <li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa->kelas->nama_kelas}}</li>
                     <li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa->jurusan}}</li>
                 </ul>
