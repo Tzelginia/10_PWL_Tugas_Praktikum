7bf8788911610301ba71cb37f02398a2b83552b5
tzelginianc <tzelginia78@gmail.com>
2022-04-24 15:36:56 +0700

    menambahkan package DomPDF

--- config/app.php
+++ config/app.php
@@ -135,6 +135,7 @@ return [
     */
 
     'providers' => [
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Laravel Framework Service Providers...
@@ -189,6 +190,7 @@ return [
     */
 
     'aliases' => [
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
